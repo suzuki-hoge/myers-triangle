e5b6a6f6f9d9018ddde743c7005fe25bb78b0ba8
suzuki-hoge <user.ryo@gmail.com>
2021-08-23 10:35:34 +0900

    create php sample project.

---
+++ php/src/Checker.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Myers;
+
+class Checker
+{
+    // 引数と戻り値は変更してはいけない
+    public static function apply(string $v): string
+    {
+        // 実装はクラス・メソッド・例外など何をどう使っても良い
+        return '正三角形';
+    }
+}
