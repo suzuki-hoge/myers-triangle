01ba6ec0e8bdcbe26eb45b95cc2f0c2690d05cea
kakiuchi <kakiuchi@miraito-inc.co.jp>
2021-08-29 20:57:34 +0900

    実装とテストコード

--- php/src/Checker.php
+++ php/src/Checker.php
@@ -6,10 +6,32 @@ namespace Myers;
 
 class Checker
 {
-    // 引数と戻り値は変更してはいけない
     public static function apply(string $v): string
     {
-        // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return '正三角形';
+        $array =  array_map('intval', explode(' ', $v));
+
+        $max = 0;
+        $sum = 0;
+        foreach ($array as $value) {
+            if ($max < $value) {
+                $sum += $max;
+                $max = $value;
+            } else {
+                $sum += $value;
+            }
+        }
+
+        if ($max >= $sum) {
+            return '不成立';
+        }
+
+        if (count(array_unique($array)) === 1) {
+            return '正三角形';
+        }
+
+        if (count(array_unique($array)) === 2) {
+            return '二等辺三角形';
+        }
+        return '不等辺三角形';
     }
 }
