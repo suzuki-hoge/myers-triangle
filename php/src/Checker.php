efee50f96b164c5be9199de871f13f6c98e68dd8
soonoki <soonoki@miraito-inc.co.jp>
2021-08-29 18:05:10 +0900

    PHPでほげさんの三角形を実装

--- php/src/Checker.php
+++ php/src/Checker.php
@@ -4,12 +4,71 @@ declare(strict_types=1);
 
 namespace Myers;
 
+use InvalidArgumentException;
+
 class Checker
 {
     // 引数と戻り値は変更してはいけない
     public static function apply(string $v): string
     {
-        // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return '正三角形';
+        $params = self::convertParams($v);
+
+        if (! self::canCreateTriangle($params)) {
+            return '三角形を作れない';
+        }
+
+        if (self::canCreateEquilateralTriangle($params)) {
+            return '正三角形';
+        }
+
+        if (self::canCreateIsoscelesTriangle($params)) {
+            return '二等辺三角形';
+        }
+
+        return '不等辺三角形';
+    }
+
+    private static function convertParams(string $v): array
+    {
+        $params = explode(' ', $v);
+
+        self::validateParams($params);
+
+        // 処理がしやすくなるので値の降順に並べておく
+        rsort($params);
+
+        return $params;
+    }
+
+    private static function validateParams(array $params): void
+    {
+        // 引数の数が3以外であればエラー
+        if (count($params) !== 3) {
+            throw new InvalidArgumentException('引数は3つ必要です');
+        }
+
+        // 引数の値が正の整数以外であればエラー
+        if (in_array(false, array_map(static fn ($v) => ctype_digit($v) && (int) $v > 0, $params), true)) {
+            throw new InvalidArgumentException('引数は正の整数を指定してください');
+        }
+    }
+
+    private static function canCreateTriangle(array $params): bool
+    {
+        // 最長辺 < 他２辺の合計 なら三角形を作れる
+        return $params[0] < ($params[1] + $params[2]);
+    }
+
+    private static function canCreateEquilateralTriangle(array $params): bool
+    {
+        // 全ての辺の長さが等しければ正三角形
+        // 降順でソート済みなので、最初と最後が等しければすべて等しい
+        return $params[0] === $params[2];
+    }
+
+    private static function canCreateIsoscelesTriangle(array $params): bool
+    {
+        // いずれかの辺の長さが等しければ二等辺三角形
+        return $params[0] === $params[1] || $params[1] === $params[2];
     }
 }
--- php/tests/CheckTest.php
+++ php/tests/CheckTest.php
@@ -5,25 +5,95 @@ declare(strict_types=1);
 namespace Myers;
 
 use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
 
 class CheckTest extends TestCase
 {
     /**
-     * @test
-     * @dataProvider apply_dp
+     * @dataProvider 正三角形プロバイダ
      */
-    public function apply(string $v, string $exp)
+    public function test_正三角形(string $input): void
     {
-        // 必要だと考えたテストを実装する
-        // テストメソッドを分けたりしても良い
-        $this->assertSame($exp, Checker::apply($v));
+        $this->assertSame('正三角形', Checker::apply($input));
     }
 
-    function apply_dp(): array
+    public function 正三角形プロバイダ(): iterable
     {
-        return [
-            '正三角形のパターン 1' => ['3 3 3', '正三角形'],
-            '正三角形のパターン 2' => ['5 5 5', '正三角形'],
-        ];
+        yield '辺の長さが3' => ['3 3 3'];
+        yield '辺の長さが10' => ['10 10 10'];
+    }
+
+    /**
+     * @dataProvider 二等辺三角形プロバイダ
+     */
+    public function test_二等辺三角形(string $input): void
+    {
+        $this->assertSame('二等辺三角形', Checker::apply($input));
+    }
+
+    public function 二等辺三角形プロバイダ(): iterable
+    {
+        yield '1つ目の辺が異なる長さ' => ['1 3 3'];
+        yield '2つ目の辺が異なる長さ' => ['6 4 6'];
+        yield '3つ目の辺が異なる長さ' => ['9 9 4'];
+        yield '二等辺の方が長い' => ['2 5 5'];
+        yield '二等辺の方が短い' => ['10 6 6'];
+    }
+
+    /**
+     * @dataProvider 不等辺三角形プロバイダ
+     */
+    public function test_不等辺三角形(string $input): void
+    {
+        $this->assertSame('不等辺三角形', Checker::apply($input));
+    }
+
+    public function 不等辺三角形プロバイダ(): iterable
+    {
+        yield '大>中>小' => ['10 9 8'];
+        yield '大>小>中' => ['9 5 7'];
+        yield '中>大>小' => ['4 5 3'];
+        yield '中>小>大' => ['7 5 10'];
+        yield '小>大>中' => ['2 4 3'];
+        yield '小>中>大' => ['3 4 5'];
+    }
+
+    /**
+     * @dataProvider 三角形を作れないプロバイダ
+     */
+    public function test_三角形を作れない(string $input): void
+    {
+        $this->assertSame('三角形を作れない', Checker::apply($input));
+    }
+
+    public function 三角形を作れないプロバイダ(): iterable
+    {
+        // 最長辺 >= 他２辺の合計なら三角形を作れない
+        yield '1つ目が最長辺' => ['10 5 4'];
+        yield '2つ目が最長辺' => ['1 4 2'];
+        yield '3つ目が最長辺' => ['2 4 10'];
+        yield '他２辺の値が等しい' => ['1 1 3'];
+        yield '最長辺=他２辺の合計' => ['1 2 3'];
+    }
+
+    /**
+     * @dataProvider 引数異常プロバイダ
+     */
+    public function test_引数異常(string $input): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Checker::apply($input);
+    }
+
+    public function 引数異常プロバイダ(): iterable
+    {
+        yield '引数の数が2つ' => ['10 5'];
+        yield '引数の数が4つ' => ['10 5 4 3'];
+        yield '区切り文字が異なる（全角空白）' => ['1　4　2'];
+        yield '数字以外の値が含まれている' => ['1 2 a'];
+        yield '小数が含まれている' => ['1 2 2.5'];
+        yield '負数が含まれている' => ['1 -1 2'];
+        yield '0が含まれている' => ['1 2 0'];
     }
 }
