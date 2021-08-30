f85c8ee60d556a40245fcec727259eda36d83f36
suzuki-hoge <user.ryo@gmail.com>
2021-08-30 10:57:17 +0900

    implement php.

--- php/src/Checker.php
+++ php/src/Checker.php
@@ -9,7 +9,25 @@ class Checker
     // 引数と戻り値は変更してはいけない
     public static function apply(string $v): string
     {
-        // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return '正三角形';
+        if (trim($v) === '') {
+            return '入力がありません';
+        }
+
+        $xs = explode(' ', $v);
+
+        if (in_array(false, array_map('ctype_digit', $xs))) {
+            return '正の整数以外が含まれています';
+        }
+
+        $ns = array_map('intval', $xs);
+        if (in_array(true, array_map(fn($n) => $n === 0, $ns))) {
+            return '0 が含まれています';
+        } else if (in_array(true, array_map(fn($n) => 99 < $n, $ns))) {
+            return '99 より大きい数が含まれています';
+        } else if (count($ns) !== 3) {
+            return '整数が 3 つではありません';
+        } else {
+            return Triangle::check($ns[0], $ns[1], $ns[2]);
+        }
     }
 }
