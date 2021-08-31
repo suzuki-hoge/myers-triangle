0ba7523c8e9e8a67e0983d9345bba9fddd5c45b2
eri <76990183+erimemo@users.noreply.github.com>
2021-08-31 10:28:56 +0900

    create test

--- php/src/Checker.php
+++ php/src/Checker.php
@@ -10,6 +10,19 @@ class Checker
     public static function apply(string $v): string
     {
         // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return '正三角形';
+        $line = explode(" ", $v);
+        sort($line);
+
+        if($line[0] > 0 && $line[1] > 0 && $line[2] > 0) {
+            if(count(array_unique($line)) === 1) {
+                return '正三角形';
+            } else if(count(array_unique($line)) === 2) {
+                return '二等辺三角形';
+            } else if($line[0] + $line[1] > $line[2]) {
+                return '不等辺三角形';
+            }
+        }
+        return '不成立';
+
     }
 }
